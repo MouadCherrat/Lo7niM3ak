b0bada6d6ff4a476fe8ba0f7ed5bad9c654d4a2b
MouadCherrat <mouad19cherrat@gmail.com>
2024-11-02 11:27:15 +0100

    initializing project

---
+++ src/main/java/com/example/lo7nim3ak/Lo7niM3akApplication.java
@@ -0,0 +1,13 @@
+package com.example.lo7nim3ak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lo7niM3akApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lo7niM3akApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lo7nim3ak/Lo7niM3akApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lo7nim3ak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lo7niM3akApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
