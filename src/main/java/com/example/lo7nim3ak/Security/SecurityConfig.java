f0de9b5913cf51ab0636e617f78f6964e4fe1609
khalil-souidi <khalilsouidi9@gmail.com>
2024-11-29 17:39:34 +0100

    disable keycloak

--- src/main/java/com/example/lo7nim3ak/Security/SecurityConfig.java
+++ src/main/java/com/example/lo7nim3ak/Security/SecurityConfig.java
@@ -1,79 +1,79 @@
-package com.example.lo7nim3ak.Security;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-    private final CorsConfigurationSource corsConfigurationSource;
-
-    public SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
-        this.corsConfigurationSource = corsConfigurationSource;
-    }
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource))
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/admin").hasAuthority("ROLE_driver")
-                        .requestMatchers("/api/user").hasAuthority("ROLE_passenger")
-                        .requestMatchers("/api/user").hasAuthority("ROLE_passanger")
-                        .requestMatchers("/api/v1/user/*").permitAll()
-                        .requestMatchers("/api/v1/drives/**").permitAll()
-                        .requestMatchers("/api/private").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
-    }
-
-
-    private JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            Collection<GrantedAuthority> authorities = extractAuthoritiesFromJwt(jwt);
-            logger.info("Extracted authorities: {}", authorities);
-            return authorities;
-        });
-        return jwtConverter;
-    }
-
-    private Collection<GrantedAuthority> extractAuthoritiesFromJwt(Jwt jwt) {
-        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
-        if (realmAccess != null && realmAccess.containsKey("roles")) {
-            List<String> roles = (List<String>) realmAccess.get("roles");
-            return roles.stream()
-                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                    .peek(role -> logger.info("Mapped role: {}", role))
-                    .collect(Collectors.toList());
-        }
-        return List.of();
-    }
-}
+//package com.example.lo7nim3ak.Security;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//
+//import java.util.Collection;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+//    private final CorsConfigurationSource corsConfigurationSource;
+//
+//    public SecurityConfig(CorsConfigurationSource corsConfigurationSource) {
+//        this.corsConfigurationSource = corsConfigurationSource;
+//    }
+//
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource))
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/admin").hasAuthority("ROLE_driver")
+//                        .requestMatchers("/api/user").hasAuthority("ROLE_passenger")
+//                        .requestMatchers("/api/user").hasAuthority("ROLE_passanger")
+//                        .requestMatchers("/api/v1/user/*").permitAll()
+//                        .requestMatchers("/api/v1/drives/**").permitAll()
+//                        .requestMatchers("/api/private").authenticated()
+//                        .anyRequest().permitAll()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+//                )
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        return http.build();
+//    }
+//
+//
+//    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+//        jwtConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            Collection<GrantedAuthority> authorities = extractAuthoritiesFromJwt(jwt);
+//            logger.info("Extracted authorities: {}", authorities);
+//            return authorities;
+//        });
+//        return jwtConverter;
+//    }
+//
+//    private Collection<GrantedAuthority> extractAuthoritiesFromJwt(Jwt jwt) {
+//        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+//        if (realmAccess != null && realmAccess.containsKey("roles")) {
+//            List<String> roles = (List<String>) realmAccess.get("roles");
+//            return roles.stream()
+//                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+//                    .peek(role -> logger.info("Mapped role: {}", role))
+//                    .collect(Collectors.toList());
+//        }
+//        return List.of();
+//    }
+//}
